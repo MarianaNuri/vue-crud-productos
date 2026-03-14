61eba4ba3931afca45f64690795e738a64e3013b
MarianaNuri <l24270017@tuxtla.tecnm.mx>
2026-03-14 16:21:02 -0600

    Agregar imagenes de los productos

--- src/stores/useProductoStore.js
+++ src/stores/useProductoStore.js
@@ -9,56 +9,56 @@ productos: [
 id:1,
 nombre:"Monstera Deliciosa",
 precio:350,
-imagen:"https://images.unsplash.com/photo-1501004318641-b39e6451bec6"
+imagen:"https://www.tuplanteria.com/productos/monstera-deliciosa-costilla-de-adan-o-ceriman/?srsltid=AfmBOoqgMRO0u14AXquRp10Bod36stZ-qywvw-Y_BJ0rZ34boYrgF-ui"
 },
 
 {
 id:2,
 nombre:"Suculenta Echeveria",
 precio:120,
-imagen:"https://images.unsplash.com/photo-1466692476868-aef1dfb1e735"
+imagen:"https://www.freepik.es/fotos-premium/echeveria-suculenta-olla-sobre-fondo-blanco-aislado_37561990.htm"
 },
 
 {
 id:3,
 nombre:"Cactus Decorativo",
 precio:150,
-imagen:"https://images.unsplash.com/photo-1509423350716-97f2360af2e4"
+imagen:"https://www.istockphoto.com/es/fotos/cactus-fondo-blanco"
 },
 
 {
 id:4,
 nombre:"Helecho",
 precio:200,
-imagen:"https://images.unsplash.com/photo-1502082553048-f009c37129b9"
+imagen:"https://es.dreamstime.com/helecho-de-boston-sobre-fondo-blanco-hermosa-espada-com%C3%BAn-verde-fresco-o-nephrolepis-exaltata-l-schott-cv-bostoniensis-creciendo-image217979673"
 },
 
 {
 id:5,
 nombre:"Planta Serpiente",
 precio:280,
-imagen:"https://images.unsplash.com/photo-1598887142487-9a3c1a3a5a4d"
+imagen:"https://www.freepik.es/fotos-premium/planta-serpiente-olla-grande-blanco-aislado-sobre-fondo-blanco_14721095.htm"
 },
 
 {
 id:6,
 nombre:"Pothos",
 precio:180,
-imagen:"https://images.unsplash.com/photo-1604762512526-7a29e2a87277"
+imagen:"https://es.dreamstime.com/pothos-epipremnum-aureum-aislado-en-fondo-blanco-una-vibrante-planta-de-aislada-un-ideal-para-la-decoraci%C3%B3n-del-hogar-y-el-image367532254"
 },
 
 {
 id:7,
 nombre:"Ficus Lyrata",
 precio:420,
-imagen:"https://images.unsplash.com/photo-1604762524889-3e2fcc145683"
+imagen:"https://www.freepik.es/fotos-premium/fiddle-leaf-fig-o-ficus-lyrata-aislado-fondo-blanco-brillante-planta-maceta-camino-recorte_92041651.htm"
 },
 
 {
 id:8,
 nombre:"Lavanda",
 precio:160,
-imagen:"https://images.unsplash.com/photo-1508780709619-79562169bc64"
+imagen:"https://www.istockphoto.com/es/foto/lavanda-aislada-sobre-fondo-blanco-gm1039673830-278324590"
 }
 
 ]
