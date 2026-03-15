6c0bc8b14bfe2d8cfd8ac8ad21f5a9a7b0837668
MarianaNuri <l24270017@tuxtla.tecnm.mx>
2026-03-15 13:43:14 -0600

    estructura en Producto form para que se pueda publicar

---
+++ src/views/ProductoForm.vue
@@ -0,0 +1,24 @@
+<template>
+
+<div class="formulario">
+
+<h1>Formulario de Producto</h1>
+
+<p>Aquí se crearán o editarán productos.</p>
+
+</div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+.formulario{
+padding:40px;
+text-align:center;
+}
+
+</style>
